950c24b304719f4a7923ea76a5c29e2bc209ce3d
IamLAM <ingeniero@luismonge.com.mx>
2019-04-23 00:22:02 -0700

    roman numbers fixed. I had included a extra number

--- code.js
+++ code.js
@@ -1,7 +1,9 @@
 function convertToRoman(num) {
 
-    let romans = ["MMMCM", "MMMD", "MMMCD", "MMM", "MMCM", "MMD", "MMCD", "MM", "MCM", "MD", "MCD", "MC", "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+    let romans = ["MMMCM", "MMMD", "MMMCD", "MMM", "MMCM", "MMD", "MMCD", "MM", "MCM", "MD", "MCD", "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
     let natural = [3900, 3500, 3400, 3000, 2900, 2500, 2400, 2000, 1900, 1500, 1400, 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    console.log(natural.length);
+    console.log(romans.length);
     let nroman = "";
     let anroman = [];
     let i = 0,
@@ -10,21 +12,12 @@ function convertToRoman(num) {
     for (j = 0; j < natural.length; j++) {
         console.log("(" + j + ")" + " num: " + num + " natural[j] " + natural[j] + " romans[j] " + romans[j]);
         while (num >= natural[j]) {
-
-
             num = num - natural[j];
             nroman = nroman + romans[j];
             console.log(num + "-" + natural[j] + "R= " + nroman);
 
         }
-
-
-
-        return nroman;
     }
-
-
-
 }
 
-convertToRoman(3900);
+convertToRoman(18);
