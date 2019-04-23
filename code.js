80972fbde8947d981969cb59986b031f84abf348
IamLAM <ingeniero@luismonge.com.mx>
2019-04-23 00:12:41 -0700

    I included romans numbers until 3999

--- code.js
+++ code.js
@@ -1,35 +1,30 @@
 function convertToRoman(num) {
 
-    let romans = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
-    let natural = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    let romans = ["MMMCM", "MMMD", "MMMCD", "MMM", "MMCM", "MMD", "MMCD", "MM", "MCM", "MD", "MCD", "MC", "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+    let natural = [3900, 3500, 3400, 3000, 2900, 2500, 2400, 2000, 1900, 1500, 1400, 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
     let nroman = "";
     let anroman = [];
     let i = 0,
         j = 0;
 
-    for (j = 0; j <= natural.length; j++) {
-        console.log("(" + j + ")" + " num: " + num + " natural[i] " + natural[i] + " romans[i] " + romans[i]);
-        if (num >= natural[i]) {
-            while (num >= natural[i]) {
+    for (j = 0; j < natural.length; j++) {
+        console.log("(" + j + ")" + " num: " + num + " natural[j] " + natural[j] + " romans[j] " + romans[j]);
+        while (num >= natural[j]) {
 
 
-                num = num - natural[i];
-                nroman = nroman + romans[i];
-                console.log(num + "-" + natural[i] + "R= " + nroman);
-                if (natural[i] != 1)
-                    i++;
+            num = num - natural[j];
+            nroman = nroman + romans[j];
+            console.log(num + "-" + natural[j] + "R= " + nroman);
 
-            }
-        } else {
-            i++;
         }
 
 
 
+        return nroman;
     }
 
 
 
 }
 
-convertToRoman(5);
+convertToRoman(3900);
