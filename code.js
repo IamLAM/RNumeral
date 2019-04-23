6e2d4e9075c25cad8305bf34abbe9a30ac7fda3b
IamLAM <ingeniero@luismonge.com.mx>
2019-04-22 23:50:12 -0700

    some issues,

--- code.js
+++ code.js
@@ -1,30 +1,35 @@
 function convertToRoman(num) {
 
     let romans = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
-    let natural = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 1];
+    let natural = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
     let nroman = "";
     let anroman = [];
-    let i = 0;
-    while (i <= 1000) {
+    let i = 0,
+        j = 0;
 
-        if (num == natural[i]) {
-            nroman = nroman + romans[i];
-            num = num - natural[i];
-        } else if (num > natural[i]) {
-            nroman = nroman + romans[i];
-            num = num - natural[i];
+    for (j = 0; j <= natural.length; j++) {
+        console.log("(" + j + ")" + " num: " + num + " natural[i] " + natural[i] + " romans[i] " + romans[i]);
+        if (num >= natural[i]) {
+            while (num >= natural[i]) {
 
+
+                num = num - natural[i];
+                nroman = nroman + romans[i];
+                console.log(num + "-" + natural[i] + "R= " + nroman);
+                if (natural[i] != 1)
+                    i++;
+
+            }
+        } else {
+            i++;
         }
 
-        i++;
+
+
     }
 
-    num = nroman;
-    console.log(num);
-
-
-    // return num;
 
 
 }
-convertToRoman(50);
+
+convertToRoman(5);
