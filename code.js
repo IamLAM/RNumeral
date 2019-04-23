4b63adfc5ce3b801173ef4cfc5e3079e5de29bd2
IamLAM <ingeniero@luismonge.com.mx>
2019-04-22 22:09:51 -0700

    I created a loop and condition

--- code.js
+++ code.js
@@ -3,8 +3,20 @@ function convertToRoman(num) {
     let romans = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
     let natural = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 1];
     let nroman;
+    let anroman = [];
+    let i = 1;
+    while (i <= 1000) {
 
+        if (num >= natural[i]) {
+            nroman += romans[i];
+            num = n - natural[i];
 
+        }
+        i++;
+    }
+
+    num = nroman;
+    console.log(num);
 
 
     // return num;
